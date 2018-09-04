e5e716cccdb695de1539d4dad575abec2770cf29
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-09-04 18:23:57 -0400

    backend offers fallback options to greetings and uses user instance methods to detect matches

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -13,26 +13,29 @@ class TriggersController < ApplicationController
     end
 
     def find_answer
-        #route ??
+        #POST '/find-answer'
         user_message = params[:user_message]
         bot_url_id = params[:bot_url_id]
         @user = User.find_by(bot_url_id: bot_url_id)
         
-        answer = {answer: find_trigger_match(user_message, @user)}
+        #only recognises exact trigger matches, else returns "I don't understand"
+        #answer = find_trigger_match(user_message, @user)
+        
+        #recognises general greetings and greets back if no exact trigger matches. Fallback "I don't understand still there"
+        answer = @user.respond_to_message(user_message)
         render json: answer
     end
 
     private
     def find_trigger_match(user_message, user)
-        #byebug
         answer = {text: "I don't understand"}
         user.triggers.map do |trigger|
             if trigger.text == user_message
                 random_index = rand(0..trigger.responses.length-1)
-                answer = trigger.responses[random_index]
+                answer = {text: trigger.responses[random_index].text}
             end
         end
         return answer
-
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,47 @@
 class User < ApplicationRecord
     has_many :triggers
     has_many :responses, through: :triggers
-
     accepts_nested_attributes_for :triggers
     #triggers accept nested attr for :responses
-
     has_secure_password
-
     validates :username, presence: true, uniqueness: true
+
+    @@GREETINGS = ["hi", "hey", "hello", "sup", "yo"]
+    @@GOODBYES = ["bye", "byebye", "goodbye", "seeya"]
+
+    def respond_to_message(user_message)
+        msg = user_message.downcase.gsub(/[^0-9a-z]/i, '')
+        answer = nil
+        #first check if any user triggers match message
+        self.triggers.map do |trigger|
+            #implement some fancier fuzzy string matching
+            if trigger.text.downcase.gsub(/[^0-9a-z]/i, '') == msg
+                random_index = rand(0..trigger.responses.length - 1)
+                answer = trigger.responses[random_index].text
+            end
+        end
+        if answer.nil?
+            #detect if the message was a greeting
+            answer = self.detect_greetings_and_goodbyes(msg)
+        end
+        
+        if answer.nil?
+            #fallback 
+            answer = "I didn't understand that"
+        end
+        return {text: answer}
+    end
+
+    def detect_greetings_and_goodbyes(msg)
+        if @@GREETINGS.include?(msg)
+            random_i = rand(0..@@GREETINGS.length - 1)
+            return @@GREETINGS[random_i]
+        elsif @@GOODBYES.include?(msg)
+            random_i = rand(0..@@GOODBYES.length - 1)
+            return @@GOODBYES[random_i]
+        else
+            return nil
+        end
+    end
+
 end

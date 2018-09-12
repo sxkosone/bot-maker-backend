77ff3d4ae51412da471688875a5d31014f2521a8
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-09-12 10:58:44 -0400

    fixed extra end in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show, :index, :update] do #take out index after development!
     #nested routes: users have triggers, triggers have responses
     #resources :triggers, shallow: true do #may not need all routes for triggers!
-    end
   end
   resources :bots, only: [:destroy]
   #logging in
